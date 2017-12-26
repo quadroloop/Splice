8d29ca758ace158e3afd39b37e517b0f8ac0a42b
quadroloop <nplixel@gmail.com>
2017-12-26 00:51:44 -0800

    add css, and js external resource

--- splice.php
+++ splice.php
@@ -390,7 +390,7 @@ $(document).ready( function() {
     
         $( cont ).addClass( 'wait' );
             
-        $.post( 'Foldertree.php', { dir: root }, function( data ) {
+        $.post( 'splice_dir_tree.php', { dir: root }, function( data ) {
     
             $( cont ).find( '.start' ).html( '' );
             $( cont ).removeClass( 'wait' ).append( data );
