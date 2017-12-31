c96aa3b9e89d8e81b6d11f32d32159b015282c9b
quadroloop <nplixel@gmail.com>
2017-12-31 03:13:30 +0300

    Removed stray files

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$it = new RecursiveTreeIterator(new RecursiveDirectoryIterator("./", RecursiveDirectoryIterator::SKIP_DOTS));
-foreach($it as $path) {
-  echo "<li><a name=".$path.">".$path."</a></li>";
-}
-exit();
